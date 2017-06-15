6f83f968a1830e7e247958c0f45ec49406ee71eb
Eric Schwartz <eric.schwartz@theironyard.com>
2017-06-15 10:39:29 -0400

    initial commit

---
+++ assets/js/create.js
@@ -0,0 +1,35 @@
+/**
+ * Use the jQuery Validate plugin to add validation to the form
+ *
+ * Here's what this you will need to do:
+ *
+ * 1. Include the following jQuery Validate JavaScript in layout.ejs
+ *    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+ *
+ * 2. Use jQuery validate and add validation to the form with the following requirements
+ *    First Name - required, at least 2 characters
+ *    Last Name  - required, at least 2 characters
+ *	  start_date - make sure date is yyyy-mm-dd
+ *	  ADD any other validation that makes you happy
+ *
+ * 3. Use a custom message for one validation
+ *
+ * 4. Make the color of the error text red
+ *
+ *
+ *
+ * Here's the documentation you need:
+ * https://jqueryvalidation.org/validate/
+ * https://jqueryvalidation.org/documentation/#link-list-of-built-in-validation-methods
+ *
+ */
+
+(function(){
+
+  $(function(){
+
+  	//code goes here
+
+  })
+
+})();
---
+++ assets/js/update.js
@@ -0,0 +1,51 @@
+/**
+ * Use the jQuery Validate and the bootstrap-select plugin to enhance this page
+ *
+ * Here's what this you will need to do:
+ *
+ * 1. When the page is loaded all form fields should be disabled except
+ *    for the dropdown to select a student
+ *
+ * 2. Using the bootstrap-selct plugin render dropdown on the page
+ *
+ * 3. Use the live search functionality to make the dropdown searchable
+ *
+ * 4. Add the user glyphicons next to each student in the list
+ *
+ * 6. Add a menu header to the dropdown
+ *
+ * 7. Customize further with anything you find intersting
+ *
+ * 8. When an student is selected the form fields should be enabled
+      and populated with the data for the selected student
+ *
+ * 9. Use jQuery validate and add validation to the form with the following requirements
+ *    First Name - required, at least 2 characters
+ *    Last Name  - required, at least 2 characters
+ *	  start_date - make sure date is yyyy-mm-dd
+ *	  ADD any other validation that makes you happy
+ *
+ * 10. Make the color of the error text red
+ *
+ *
+ *
+ * Here's the documentation you need:
+ * https://jqueryvalidation.org/validate/
+ * https://jqueryvalidation.org/documentation/#link-list-of-built-in-validation-methods
+ * https://silviomoreto.github.io/bootstrap-select/
+ * https://silviomoreto.github.io/bootstrap-select/examples/
+ * http://getbootstrap.com/components/#glyphicons
+ * https://api.jquery.com/jQuery.get/
+ * http://stackoverflow.com/questions/9807426/use-jquery-to-re-populate-form-with-json-data
+ *
+ */
+
+ (function(){
+
+   $(function(){
+
+    //code goes here
+
+   })
+
+ })();
